08fff26eec698955d0353138ea5f7729ab97256f
blablupcom <o.volodin@yahoo.com>
2015-07-23 20:26:33 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -36,6 +36,7 @@ def validateURL(url):
         try:
             title = html.title.text
         except: pass
+        print title
         while r.status_code == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
